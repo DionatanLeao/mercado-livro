3a7ea116031bc2e575b73daab89c40823a7d2691
DionatanLeao <dionatan.ti@outlook.com>
2023-01-10 16:06:40 -0300

    Configuring the Swagger

---
+++ src/main/kotlin/com/mercadolivro/config/SwaggerConfig.kt
@@ -0,0 +1,20 @@
+package com.mercadolivro.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info().title("Mercado Livro")
+                    .description("Api do Mercado Livro")
+                    .version("v1.0.0")
+            )
+    }
+}
